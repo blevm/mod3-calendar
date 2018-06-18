72a75d26d9902511f9cd81d72e48cf6ac9e3df64
Mendy Wolosow <mendywoly@gmail.com>
2018-06-18 15:59:35 -0400

    Added event appending

--- mod3-calendar-backend/app/controllers/api/v1/events_controller.rb
+++ mod3-calendar-backend/app/controllers/api/v1/events_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::EventsController < ApplicationController
 
+  # def index
+  #   events = Event.all
+  #   render json: events
+  # end
+
   def index
-    events = Event.all
+    current_user = User.first
+    events = current_user.events
     render json: events
   end
+
 
 end
